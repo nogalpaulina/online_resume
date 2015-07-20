61bd1380a96ad4d0fb207a29fca0c20fbbc8a744
Paulina Nogal <nogalpaulina@gmail.com>
2015-07-20 15:40:24 -0400

    Project 2 Online Resume Submission

--- js/helper.js
+++ js/helper.js
@@ -1,22 +1,8 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLwebsite = '<li class="flex-item"><span class="orange-text">website</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
@@ -37,10 +23,11 @@ var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectURL = '<a href="%data%">';
+var HTMLprojectTitle = '%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img class="project-image" src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -49,7 +36,7 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h2>Online Classes</h2>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -60,17 +47,17 @@ var googleMap = '<div id="map"></div>';
 
 
 /*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+The International Name
 */
-$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
-});
+// $(document).ready(function() {
+//   $('button').click(function() {
+//     var iName = inName() || function(){};
+//     $('#name').html(iName);  
+//   });
+// });
 
 /*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+Click Locations
 */
 clickLocations = [];
 
@@ -85,22 +72,15 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+    logClicks(loc.pageX, loc.pageY)
 });
 
-
-
 /*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
+Map
 */
-var map;    // declares a global map variable
 
+var map;
 
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
 function initializeMap() {
 
   var locations;
@@ -109,82 +89,65 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-
   map = new google.maps.Map(document.querySelector('#map-div'), mapOptions);
 
-
   /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
+  locationFinder()
   */
+
   function locationFinder() {
 
-    // initializes an empty array
     var locations = [];
 
-    // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(bio.contactInfo.location);
 
-    // iterates through school locations and appends each location to
-    // the locations array
     for (var school in education.schools) {
       locations.push(education.schools[school].location);
     }
 
-    // iterates through work locations and appends each location to
-    // the locations array
     for (var job in work.jobs) {
-      locations.push(work.jobs[job].location);
+      if (work.jobs[job].locations.length > 0) {
+        for (var location in work.jobs[job].locations) { 
+          locations.push(work.jobs[job].locations[location]);
+        }
+      }
     }
 
     return locations;
   }
 
   /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
+  createMapMarker
   */
   function createMapMarker(placeData) {
 
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+    var lat = placeData.geometry.location.lat();
+    var lon = placeData.geometry.location.lng();
+    var name = placeData.formatted_address;
+    var bounds = window.mapBounds;
 
-    // marker is an object with additional data about the pin for a single location
     var marker = new google.maps.Marker({
       map: map,
       position: placeData.geometry.location,
       title: name
     });
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
     var infoWindow = new google.maps.InfoWindow({
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map,marker);
     });
 
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
+
     bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
+
     map.fitBounds(bounds);
-    // center the map
+
     map.setCenter(bounds.getCenter());
   }
 
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
@@ -192,51 +155,32 @@ function initializeMap() {
   }
 
   /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
+  pinPoster(locations)
   */
   function pinPoster(locations) {
 
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
     var service = new google.maps.places.PlacesService(map);
 
-    // Iterates through the array of locations, creates a search object for each location
     for (var place in locations) {
 
-      // the search request object
       var request = {
         query: locations[place]
       };
 
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
+      // Searches the Google Maps API 
       service.textSearch(request, callback);
     }
   }
 
-  // Sets the boundaries of the map based on pin locations
   window.mapBounds = new google.maps.LatLngBounds();
 
-  // locations is an array of location strings returned from locationFinder()
   locations = locationFinder();
 
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
   pinPoster(locations);
-
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+window.addEventListener('load', initializeMap);
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
-
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,296 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//  JSON 
+
+var bio = {
+	"name" : "Paulina Nogal",
+	"role" : "Web developer",
+	"welcomeMessage" : "I'm a scrupulous and ambitious person. My goal is to learn and grow every day. I like to be challenged and have my efforts count. I enjoy working in groups with smart and creative people whom I can learn from. In the short-term, I continue to develop valuable skills that will get me closer to having a meaningful career as a web developer.",
+	"contactInfo" : {
+				"email" : "panokadev@gmail.com",
+				"website" : "www.paulinanogal.com",
+				"github" : "nogalpaulina",
+				"twitter" : "@pjnogal",
+				"location" : "Newport, RI"
+	},
+	"skills" : ["HTML5", "CSS3", "JavaScript", "Responsive design", "Bootstrap", "Project management", "Time management"],
+	"pictureUrl" : "images/profile2.jpg"
+}
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Au Pair in America",
+			"title" : "Au Pair",
+			"locations" : ["Denver CO ", "Tiburon CA ", "Fairfield CT"],
+			"dates" : "December 2012 - December 2014",
+			"description" : "I've been taking care of kids in America. I have had to be responsible and well-organized every day. I have had to quickly adjust to new environments and lifestyles."
+		},
+		{
+			"employer" : "Swojski Smak Restaurant",
+			"title" : "Manager Assistant",
+			"locations" : ["Gdansk, Poland"],
+			"dates" : "September 2009 - December 2012",
+			"description" : "Solving employee's issues and customer service, organizing daily schedules, and ordering restaurant supplies. Serving to mostly international customers."
+		},
+		{
+			"employer" : "Orange Telecommunications",
+			"title" : "Customer Service Representative",
+			"locations" : ["Gdansk, Poland"],
+			"dates" : "January 2010 - June 2012",
+			"description" : "Assisting customers to keep, extend or change their contracts with the company. Adjusting new plans and contracts for each customer's needs."
+		}
+	]
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Vilabam Greenhouses",
+			"url" : "http://paulinanogal.com/greenhouse/",
+			"dates" : "2015",
+			"description" : "Modern, responsive business website featuring a video background. Technologies used: HTML5, CSS3, Bootstrap",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/07/greenhouse-560x450.jpg"]
+		},
+		{
+			"title" : "Dog adoptions",
+			"url" : "http://paulinanogal.com/dog_adoptions/",
+			"dates" : "2015",
+			"description" : "Website promoting dog adoptions with parallax scrolling and infographic. Technologies used: HTML5, CSS3, Google Fonts, Font Awesome, Bootstrap",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/06/pic-560x450.png"]
+		},
+		{
+			"title" : "Viss Schubben Restaurant",
+			"url" : "http://paulinanogal.com/nordic_cuisine/",
+			"dates" : "2015",
+			"description" : "Responsive website for nordic quisine family restaurant. Technologies used: HTML5, CSS3, Bootstrap, Google Fonts, Font Awesome",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/05/vis-560x450.png"]
+		},
+		{
+			"title" : "Law office of J. & K. Plowrells",
+			"url" : "http://paulinanogal.com/lawyer/",
+			"dates" : "2015",
+			"description" : "Clean, modern website for private law office. Technologies used: HTML5, CSS3, Bootstrap, Google Fonts",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/05/lawyer-560x450.png"]
+		},
+		{
+			"title" : "Toughron, International business training leaders",
+			"url" : "http://paulinanogal.com/toughron/",
+			"dates" : "2015",
+			"description" : "Modern, responsive business website featuring a video background. Technologies used:  HTML5, CSS3, Bootstrap, Google Fonts",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/06/toughron-560x450.png"]
+		},
+		{
+			"title" : "Cliff Walk",
+			"url" : "http://paulinanogal.com/cliff/",
+			"dates" : "2015",
+			"description" : "Responsive website for Newport's Cliff Walk. Technologies used: HTM5, CSS3, Bootstrap",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/05/cliff-560x450.png"]
+		},
+		{
+			"title" : "Photojack",
+			"url" : "http://paulinanogal.com/city/",
+			"dates" : "2015",
+			"description" : "Landing page for New York photographer featuring parallax image scrolling. Technologies used: HTML5, CSS3, Bootstrap, Google Fonts",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/05/city-560x450.png"]
+		},
+		{
+			"title" : "Henry Pearl Ballons",
+			"url" : "http://paulinanogal.com/balloon/",
+			"dates" : "2015",
+			"description" : "Landing page for air balloon rides over the Grand Canyon. Technologies used: HTML5, CSS3, Bootstrap, Google Fonts",
+			"images" : ["http://paulinanogal.com/wp-content/uploads/2015/05/balloon-560x450.jpg"]
+		}
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "University of Gdansk",
+			"location" : "Gdansk, Poland",
+			"degree" : "Master's",
+			"major" : "Management",
+			"dates" : "September 2010 - July 2012",
+			"url" : "http://www.wzr.ug.edu.pl/biznes/index.php?i=28&o=24,28"
+		},
+		{
+			"name" : "Academy of turism and hotel management in Gdansk",
+			"location" : "Gdansk, Poland",
+			"degree" : "Bachelor's",
+			"major" : "SPA, Wellness and general health",
+			"dates" : "September 2007 - July 2010",
+			"url" : "http://wstih.pl/en/news"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Front-end Web Developer Nanodegree",
+			"school" : "Udacity",
+			"dates" : "July 2015",
+			"url" : "https://www.udacity.com"
+		},
+		{
+			"title" : "English language (12 credits)",
+			"school" : "UCLA",
+			"dates" : "2013 - 2014",
+			"url" : "https://www.uclaextension.edu/pages/fos/esl.aspx"
+		}
+	]
+}
+
+// BIO
+
+bio.display = function() {
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").prepend(formattedRole);
+
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	$("#header").prepend(formattedName);
+
+	var formattedPhoto = HTMLbioPic.replace("%data%", bio.pictureUrl);
+	$("#header").append(formattedPhoto);
+
+	var formattedMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedMessage);
+
+	var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
+	$("#topContacts").append(formattedEmail);
+
+	var formattedWebsite = HTMLwebsite.replace("%data%", bio.contactInfo.website);
+	$("#topContacts").append(formattedWebsite);
+
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contactInfo.github);
+	$("#topContacts").append(formattedGithub);
+
+	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
+	$("#topContacts").append(formattedTwitter);
+
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
+	$("#topContacts").append(formattedLocation);
+
+
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[5]);
+		$("#skills").append(formattedSkill);
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[6]);
+		$("#skills").append(formattedSkill);
+	}
+}
+bio.display();
+
+// WORK
+
+work.display = function() {
+	for (var job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].locations);
+		$(".work-entry:last").append(formattedLocation);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
+}
+work.display();
+
+
+// PROJECTS
+
+projects.display = function() {
+	for (var project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedURL = HTMLprojectURL.replace("%data%", projects.projects[project].url);
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		var formattedURLtitle = formattedURL + formattedTitle;
+		$(".project-entry:last").append(formattedURLtitle);
+
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+
+		if (projects.projects[project].images.length > 0) {
+			for (var image in projects.projects[project].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
+}
+projects.display();
+
+// EDUCATION
+
+education.display = function() {
+	for (var school in education.schools) {
+		$("#education").append(HTMLschoolStart);
+
+		var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formattedNameDegree = formattedName + formattedDegree;
+		$(".education-entry:last").append(formattedNameDegree);
+		
+		var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		$(".education-entry:last").append(formattedDates);
+		
+		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		$(".education-entry:last").append(formattedLocation);
+		
+		var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+		$(".education-entry:last").append(formattedMajor);
+	}
+
+	$("#education").append(HTMLonlineClasses);
+
+	for (var course in education.onlineCourses) {
+		$("#education").append(HTMLschoolStart);
+
+		var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+		var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+		var formattedTitleSchool = formattedTitle + formattedSchool;
+		$(".education-entry:last").append(formattedTitleSchool);
+
+		var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+		$(".education-entry:last").append(formattedDates);
+
+		var formattedUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+		$(".education-entry:last").append(formattedUrl);
+	}
+}
+education.display();
+
+// GOOGLE MAP
+
+$("#map-div").append(googleMap);
+
+// INTERNATIONALIZED NAME
+
+// $("#main").append(internationalizeButton);
+
+// function inName() {
+// 	var splitName = bio.name.split(" ");
+// 	var firstName = splitName[0].charAt(0).toUpperCase() + splitName[0].substring(1).toLowerCase();
+// 	var lastName = splitName[1].toUpperCase();
+// 	var fullName = firstName + " " + lastName;
+// 	return fullName;
+// }
